c9c7096e4624365458dcb08d6cbdfc3ce48f0a5a
biliqis <biliqistemmy@gmail.com>
2024-11-15 21:00:52 +0100

    new commit

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -5,8 +5,8 @@ const Headers = () => {
   return (
     <div className='flex col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20'>
       {/*--------left side --------- */}
-      <div>
-        <p>Book Appointment<br/>  With Trusted Doctors</p>
+      <div className='md:w-1/2 flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px] '>
+        <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>Book Appointment<br/>  With Trusted Doctors</p>
         <div>
             <img src={assets.group_profiles} alt=''/> 
             <p>Simply browse through our extensive list of trusted doctors,<br/>
@@ -19,8 +19,8 @@ const Headers = () => {
       </div>
 
      { /********Right side   ******/ }
-      <div>
-        <img src={assets.header_img}/>
+      <div className='md:w-1/2 relative'>
+        <img className='w-full md:absolute bottom-0 h-auto rounded-lg:' src={assets.header_img}/>
 
       </div>
  
