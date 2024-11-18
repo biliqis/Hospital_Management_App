6cb1b77cb3ad49c63346dbb545272b849fd7206e
biliqis <biliqistemmy@gmail.com>
2024-11-18 21:59:20 +0100

    new commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import SpecialityMenu from '../components/SpecialityMenu'
+import Headers from '../components/Headers'
 
 const Home = () => {
   return (
     <div>
-      
+<Headers/>
+       <SpecialityMenu/>
     </div>
   )
 }
