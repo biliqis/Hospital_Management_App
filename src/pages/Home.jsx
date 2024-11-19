148e652fa43251689eebe8572c8897fa17cbb171
biliqis <biliqistemmy@gmail.com>
2024-11-19 10:22:08 +0100

    Add the repeat auto-fill

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SpecialityMenu from '../components/SpecialityMenu'
 import Headers from '../components/Headers'
+import TopDoctors from '../components/TopDoctors'
 
 const Home = () => {
   return (
     <div>
 <Headers/>
        <SpecialityMenu/>
+       <TopDoctors/>
     </div>
   )
 }
