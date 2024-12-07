a1778426d68abb9f30d714b11e6a06aeb84778e9
biliqis <biliqistemmy@gmail.com>
2024-12-07 23:46:13 +0100

    new commit

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -15,9 +15,67 @@ const MyProfile = () => {
     gender: "Male",
     dob: "200-01-2020",
   });
+
+  const [isEdit, setIsEdit] = useState(false);
   return (
     <div>
       <img src={userData.image} />
+
+      {isEdit ? (
+        <input
+          type="text"
+          value={userData.name}
+          onChange={(e) =>
+            setUserData((prev) => ({ ...prev, name: e.target.value }))
+          }
+        />
+      ) : (
+        <p>{userData.name}</p>
+      )}
+
+      <hr />
+      <div>
+        <p> CONTACT INFORMATION</p>
+        <div>
+          <p>Email id:</p>
+          <p>{userData.email}</p>
+          <p>phone:</p>
+
+          {isEdit ? (
+            <input
+              type="text"
+              value={userData.phone}
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, phone: e.target.value }))
+              }
+            />
+          ) : (
+            <p>{userData.phone}</p>
+          )}
+          <p>Address:</p>
+          {
+            isEdit?<p>
+              <input onChange={(e)=>setUserData( prev=>({...prev, address:{...prev, address, line1:e.target.value}}))} value={userData.address.line1} type="text" />
+              <br/>
+              <input onChange={(e)=>setUserData( prev=>({...prev, address:{...prev, address, line2:e.target.value}}))} value={userData.address.line2} type="text" />
+              </p>
+            :<p>
+              {userData.address.line1}
+              <br />
+              {userData.address.line2}
+            </p>
+          }
+        </div>
+      </div>
+
+      <div>
+        <p>BASIC INFORMATION</p>
+
+        <div>
+          <p> Gender</p>
+
+        </div>
+      </div>
     </div>
   );
 };
