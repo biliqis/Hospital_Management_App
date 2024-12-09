e1668ac6425abcf874e677a298b56a150a2a70b4
biliqis <biliqistemmy@gmail.com>
2024-12-09 15:15:04 +0100

    new commit

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -6,6 +6,7 @@ import { AppContext } from "../context/AppContext";
 const Doctors = () => {
   const { speciality } = useParams();
   const [filterDoc, setFilterDoc] = useState([]);
+  const [showFilter,setShowFilter ] = useState(false)
   const { doctors } = useContext(AppContext);
   const navigate = useNavigate();
 
@@ -25,8 +26,23 @@ const Doctors = () => {
     <div>
       <p className="text-gray-600">Browse through the doctors specialist</p>
       <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
-        <div className=" flex flex-col gap-4 text-sm text-gray-600">
-          <p
+
+
+<button 
+  className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${
+    showFilter ? 'bg-primary text-white' : ''
+  }`} 
+  onClick={() => setShowFilter((prev) => !prev)}
+>
+  Filter
+</button>
+
+<div 
+  className={`flex flex-col gap-4 text-sm text-gray-600 ${
+    showFilter ? 'flex' : 'hidden sm:flex'
+  }`}
+>
+ <p
             onClick={() =>
               speciality === "General physician"
                 ? navigate("/doctors")
